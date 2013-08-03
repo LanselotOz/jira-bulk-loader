bd944091e59c02ecf84aa261515bef0d39d68f35
Alexander Dudko <alex.dudko@gmail.com>
2013-08-03 14:19:48 +0300

    argsparse moved to separate module & switched to io.open

--- bin/jira-bulk-loader.py
+++ bin/jira-bulk-loader.py
@@ -1,44 +1,16 @@
 #!/usr/bin/python
 #-*- coding: UTF-8 -*-
 
-import argparse
 from jirabulkloader.task_extractor import TaskExtractor
-from jirabulkloader.task_extractor_exceptions import TaskExtractorTemplateErrorProject, TaskExtractorJiraValidationError, TaskExtractorTemplateErrorJson, TaskExtractorJiraCreationError, TaskExtractorJiraHostProblem
-from jirabulkloader.jiraConnect import JiraConnectConnectionError
+import jirabulkloader.interface as iface
 
-prg_description="Uses template file to create many tasks in Jira at once"
+args = iface.get_options()
 
-prg_epilog="""Report bugs to: <https://bitbucket.org/oktopuz/jira-bulk-loader/issues>
-Project home page: <https://bitbucket.org/oktopuz/jira-bulk-loader>"""
-
-parser = argparse.ArgumentParser(description=prg_description, formatter_class=argparse.RawDescriptionHelpFormatter, epilog=prg_epilog)
-
-parser.add_argument('template_file', help='file containing tasks definition')
-parser.add_argument('-W', dest='project', help='Project key')
-parser.add_argument('-R', dest='priority', help='Task priority. "Medium" by default', default="Medium")
-parser.add_argument('-D', dest='dueDate', help='dueDate  (YYYY-mm-DD). For example: 2012-05-31')
-parser.add_argument('--dry', dest='dry_run', action='store_true', help='Make a dry run. It checks everything but does not create tasks', default=False)
-
-mandatory = parser.add_argument_group('mandatory arguments')
-mandatory.add_argument('-H', dest='hostname', required=True, help='Jira hostname with http:// or https://')
-mandatory.add_argument('-U', dest='username', required=True, help='your Jira username')
-mandatory.add_argument('-P', dest='password', required=True, help='your Jira password')
-
-args = parser.parse_args()
-
-
-##############################################################
-# open input file, parse and create tasks
-
-import codecs
 try:
-    f = codecs.open(args.template_file, encoding='utf-8')
-    input_text = f.read()
+    input_text = iface.get_template( args.template_file )
 except IOError as e:
     print "Template file error: %s" % e
     exit(1)
-else:
-    f.close()
 
 options = {}
 if args.dueDate: options['duedate'] = args.dueDate
@@ -46,6 +18,9 @@ if args.priority: options['priority'] = {'name':args.priority}
 if args.project: options['project'] = {'key':args.project}
 
 task_ext = TaskExtractor(args.hostname, args.username, args.password, options, dry_run = args.dry_run)
+
+from jirabulkloader.task_extractor_exceptions import TaskExtractorTemplateErrorProject, TaskExtractorJiraValidationError, TaskExtractorTemplateErrorJson, TaskExtractorJiraCreationError, TaskExtractorJiraHostProblem
+from jirabulkloader.jiraConnect import JiraConnectConnectionError
 
 try:
     print "Parsing task list.."
